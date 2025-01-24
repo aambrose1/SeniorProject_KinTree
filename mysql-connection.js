a43445ca7e5eb23bf9d38e21bb221210c1dda735
Jade Thompson <jadethompson285@gmail.com>
2025-01-24 10:23:15 -0600

    added skeleton to env file, removed old credentials from connection file

--- mysql-connection.js
+++ mysql-connection.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST || '127.0.0.1',  // localhost
-  user: process.env.DB_USER || 'root',      // Make sure DB_USER is set
-  password: process.env.DB_PASSWORD || 'root',  // Ensure DB_PASSWORD is set
-  database: process.env.DB_NAME || 'KinTreeTest',           // Ensure DB_NAME is set
+  user: process.env.DB_USER || '<Add-Username>',      // Make sure DB_USER is set
+  password: process.env.DB_PASSWORD || '<Add-Password>',  // Ensure DB_PASSWORD is set
+  database: process.env.DB_NAME || '<Add-DB-Name>',           // Ensure DB_NAME is set
   port: process.env.DB_PORT || 3306        // Port should be 3306
 });
 
