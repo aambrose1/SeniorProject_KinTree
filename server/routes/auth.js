7c5b9fb8ed685a438cca5075ccc7d46ecddd94b7
dmilsap <drm528@msstate.edu>
2025-01-29 22:19:04 -0600

    Structure Server Folder

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
-const db = require('../config/db')
+const db = require('../db/db')
 const {JWT_SECRET} = process.env;
 
 router.post('/register', async(req, res) => {
