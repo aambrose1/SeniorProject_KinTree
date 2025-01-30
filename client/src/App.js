123217d2742e3015e5458a2e44d64286105550a1
Jade Thompson <jadethompson285@gmail.com>
2025-01-30 03:41:42 -0600

    personalized base app

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
-import logo from './logo.svg';
+import logo from './assets/kintreelogo-adobe.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className={"App font-face-alata"}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Container">
+          <p>
+            Welcome to KinTree! This is the client side of the application. It's in progress.
+          </p>
+        </div>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/OwenAdams2023/SeniorProject_KinTree"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github Repository
         </a>
       </header>
     </div>
