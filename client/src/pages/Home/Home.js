e5e2bdf883dcc0471c3eb4c5bae3e5587642342e
Jade Thompson <jadethompson285@gmail.com>
2025-01-30 13:40:29 -0600

    updated home page skeleton

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import * as styles from './styles';
 
+// TODO: need to implement auth check for this page (and others), redirect to login if not authenticated
 function Home() {
     return (
         <div style={styles.DefaultStyle}>
-            <Link to="/tree" >Tree</Link>
+            <Link to="/tree" >View Tree Page</Link>
         </div>
     )
 }
