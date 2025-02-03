45ec6a7beb6378b6340991d9f22173a94f163ec8
Owen Adams <owen.adams.1234@outlook.com>
2025-02-02 16:19:18 -0800

    modified NavBar.js and added NavBar.css

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import * as styles from './styles';
 
 function NavBar() {
     return (
-        <div style={styles.DefaultStyle}>
-
-        </div>
-    )
-}
+        <nav style={styles.DefaultStyle}>
+            <ul>
+                <li>
+                    <NavLink to="/" exact className="nav-item"> Home </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/family" className="nav-item"> Family </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/tree" className="nav-item"> Tree </NavLink>
+                </li>
+                
+            </ul>
+        </nav>
+    );
+};
 
 export default NavBar;
