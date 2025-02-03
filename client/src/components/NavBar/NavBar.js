cb691f54fea6133f7fb15bba5e1ea7cdd2338bf9
Owen Adams <owen.adams.1234@outlook.com>
2025-02-02 21:22:11 -0800

    added settings and help links to NavBar.js

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { AiOutlineSetting, AiOutlineQuestion } from 'react-icons/ai'; //this is for the settings icon and help icon
 import './NavBar.css';
 //import * as styles from './styles';
 
 function NavBar() {
     return (
         <nav className="navbar">
-            <ul>
+            <ul className="nav-options-list">
                 <li>
                     <NavLink to="/profile" className="profile-icon">
                         <img src=""></img>
@@ -24,7 +25,16 @@ function NavBar() {
                 <li>
                     <NavLink to="/chat" className="nav-item"> Chat </NavLink>
                 </li>
+                
             </ul>
+            <AiOutlineSetting>
+                <NavLink to="/account_settings"></NavLink>
+            </AiOutlineSetting>
+            <AiOutlineQuestion>
+                <NavLink to="/help"></NavLink>
+            </AiOutlineQuestion>
+            
+
         </nav>
     );
 };
