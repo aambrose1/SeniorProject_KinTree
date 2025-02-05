fa381927d8c2343dcebd0490d54b2a308bf8a724
Jade Thompson <jadethompson285@gmail.com>
2025-02-04 22:41:05 -0600

    added create memory popup on home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import * as styles from './styles';
 import { ReactComponent as CalendarIcon } from '../../assets/calendar.svg';
+import { ReactComponent as PlusIcon } from '../../assets/plus-sign.svg';
 import CreateEventPopup from '../../components/CreateEvent/CreateEvent';
+import CreateMemoryPopup from '../../components/CreateMemory/CreateMemory';
 
 // TODO: need to implement auth check for this page (and others), redirect to login if not authenticated
 function Home() {
@@ -10,6 +12,7 @@ function Home() {
         <div style={styles.DefaultStyle}>
             <Link to="/tree" >View Tree Page</Link>
             <CreateEventPopup trigger={<CalendarIcon style={styles.CalendarButton} />} />
+            <CreateMemoryPopup trigger={<PlusIcon style={styles.PlusButton} />} />
         </div>
     )
 }
--- client/src/pages/Home/styles.js
+++ client/src/pages/Home/styles.js
@@ -14,3 +14,13 @@ export const CalendarButton = {
     right: '0px',
     bottom: '0px'
 }
+
+export const PlusButton = {
+    width: '40px',
+    height: '40px',
+    margin: '10px',
+    cursor: 'pointer',
+    position: 'absolute',
+    right: '0px',
+    bottom: '60px'
+}
