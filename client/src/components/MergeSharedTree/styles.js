b0ce9c04f45697742a8b7f4ee110c0a28f6ab014
Jade Thompson <jadethompson285@gmail.com>
2025-02-05 21:48:46 -0600

    added merge shared tree popup

---
+++ client/src/components/MergeSharedTree/styles.js
@@ -0,0 +1,50 @@
+export const DefaultStyle = {
+    fontFamily: 'Alata',
+};
+
+export const ButtonDivStyle = {
+    fontFamily: 'Alata',
+    display: 'flex',
+    justifyContent: 'center',
+}
+
+export const ButtonStyle = {
+    fontFamily: 'Alata',
+    backgroundColor: '#3a5a40',
+    color: 'white',
+    borderRadius: '10px',
+    border: 'none',
+    padding: '10px 30px',
+    margin: '10px',
+    cursor: 'pointer',
+}
+
+export const FormStyle = {
+    paddingTop: '0px',
+    height: '500px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    width: 'auto'
+}
+
+export const ItemStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    margin: '10px 0',
+    border: '1px solid rgb(87, 87, 87)',
+    width: '80%',
+    height: '10%',
+    padding: '10px'
+}
+
+export const CheckboxListStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    margin: '0px 30px',
+    overflow: 'scroll',
+    height: '50%',
+    border: '1px solid rgb(87, 87, 87)',
+    borderRadius: '10px'
+}
