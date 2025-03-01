9bf4d2d3fd6f47b86d2e7d651435e1fcba64567d
Jade Thompson <jadethompson285@gmail.com>
2025-03-01 13:27:04 -0600

    added login page

--- client/src/pages/Login/styles.js
+++ client/src/pages/Login/styles.js
@@ -1,9 +1,110 @@
+export const DefaultStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '10px',
+  fontFamily: 'Alata',
+  alignItems: 'center',
+}
+
 export const Container = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100vh',
-    backgroundColor: '#f5f5f5',
-    fontFamily: 'Alata'
-  };
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'flex-start',
+  minWidth: '290px',
+  width: '40%',
+  height: '80vh',
+  borderRadius: '30px',
+  backgroundColor: '#faf9f6',
+  padding: '20px 5vw',
+  marginTop: '2%',
+  boxShadow: 'gray 0px 10px 10px -8px',
+  border: '0.5px solid #000000'
+};
+
+export const Logo = {
+  width: '150px',
+  height: '150px'
+};
+
+export const ListStyle = {
+  listStyleType: 'none',
+  fontFamily: 'Alata',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  width: 'auto',
+  padding: '0'
+};
+
+export const FormStyle = {
+  padding: '2vw',
+  paddingTop: '0px',
+  width: '60%',
+  minWidth: '260px',
+  alignItems: 'center'
+}
+
+export const ItemStyle = {
+  margin: '10px 0px',
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%'
+}
+
+export const FieldStyle = {
+  borderRadius: '5px',
+  border: '1px solid #000000',
+  width: '100%',
+  height: '25px',
+  boxShadow: 'gray 0px 10px 10px -11px',
+  marginTop: '5px'
+};
+
+export const Header = {
+  margin: '0'
+}
+
+export const ButtonStyle = {
+  fontFamily: 'Alata',
+  backgroundColor: '#3a5a40',
+  color: 'white',
+  borderRadius: '10px',
+  border: 'none',
+  padding: '10px 20px',
+  margin: '10px',
+  cursor: 'pointer',
+  width: '40%',
+  height: '45px'
+}
+
+export const WhiteButtonStyle = {
+  fontFamily: 'Alata',
+  backgroundColor: 'white',
+  color: 'black',
+  borderRadius: '10px',
+  border: '1px solid #000000',
+  margin: '10px',
+  cursor: 'pointer',
+  width: '100%',
+  height: '45px',
+  padding: '10px 20px',
+}
+
+export const ButtonDivStyle = {
+  fontFamily: 'Alata',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'flex-end',
+}
+
+export const TextStyle = {
+  margin: '0',
+  fontSize: '0.7em',
+  textAlign: 'center'
+}
+
+export const LinkStyle = {
+  color: 'black',
+  textDecoration: 'none'
+}
