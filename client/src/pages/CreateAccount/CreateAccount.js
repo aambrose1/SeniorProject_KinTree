c0d047d1e6ad23e98a1e1069fdf4e702f53b560a
Owen Adams <owen.adams.1234@outlook.com>
2025-03-06 10:01:49 -0800

    added styling to styles.js and added form validation using yup

--- client/src/pages/CreateAccount/CreateAccount.js
+++ client/src/pages/CreateAccount/CreateAccount.js
@@ -1,7 +1,13 @@
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+import * as styles from './styles'
 
 function createAccount() {
     const {register, handleRegister} = useForm();
+
+    //ToDo: code linkage to backend
     const onSubmit = (data) => {
         alert(JSON.stringify(data));
         console.log(data);
@@ -9,18 +15,21 @@ function createAccount() {
 
     return (
         <form onSubmit={handleRegister(onSubmit)}>
-            <head className='acc-form-header'>
+            <head >
                 Create Account
             </head>
-            <label className='field'>
-                First Name
-                <input {...register("firstName")} type='text' />
-            </label>
-            <label className='field'>
+            <div>
+                <label >
+                    First Name
+                    <input id="firstname" {...register("firstName", { required: true })} type="text" />
+                    {errors.username && <p>{errors.username.message}</p>}
+                </label>
+            </div>
+            <label >
                 Last Name
-                <input {...register("lastName")} type='password' />
+                <input id="lastname"{...register("lastName")} type='password' />
             </label>
-            <label className='field'>
+            <label >
                 Email
                 <input {...register("emailAddress")} type='email' />
             </label>
