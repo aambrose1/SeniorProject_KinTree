e97057ab3f0b1a313f9c049f799ffbb7df9b2056
Owen Adams <owen.adams.1234@outlook.com>
2025-03-07 17:38:09 -0800

    added more styling to CreateAccount.js

--- client/src/pages/CreateAccount/CreateAccount.js
+++ client/src/pages/CreateAccount/CreateAccount.js
@@ -47,67 +47,72 @@ const CreateAccount = () => {
             <head style={styles.Header}>
                 Create Account
             </head>
-            <form onSubmit={handleSubmit(onSubmit)} style={styles.FormStyle}>
-            <div style={styles.ListStyle}>
-                <div style={styles.ItemStyle}>
-                    <label>First Name</label>
-                    <input id="firstname" {...register("firstname")} style={styles.FieldStyle}/>
-                    {errors.firstname && <p>{errors.firstname.message}</p>}
+            <div style={styles.Container}>
+                <form onSubmit={handleSubmit(onSubmit)} style={styles.FormStyle}>
+                <div style={styles.ListStyle}>
+                    <div style={styles.ItemStyle}>
+                        <label>First Name</label>
+                        <input id="firstname" {...register("firstname")} style={styles.FieldStyle}/>
+                        {errors.firstname && <p>{errors.firstname.message}</p>}
+                    </div>
+                    <div style={styles.ItemStyle}>
+                        <label>Last Name</label>
+                        <input id="lastname" {...register("lastname")} style={styles.FieldStyle}/>
+                        {errors.lastname && <p>{errors.lastname.message}</p>}
+                    </div>
+                    <div style={styles.ItemStyle}>
+                        <label>Birthdate</label>
+                        <input id="birthdate" {...register("birthdate")} style={styles.FieldStyle}/>
+                        {errors.birthdate && <p>{errors.birthdate.message}</p>}
+                    </div>
+                    <div style={styles.ItemStyle}>
+                        <label>Email</label>
+                        <input id="email" {...register("email")} style={styles.FieldStyle}/>
+                        {errors.email && <p>{errors.email.message}</p>}
+                    </div>
+                    <div style={styles.ItemStyle}>
+                        <label>Address</label>
+                        <input id="address" {...register("address")} style={styles.FieldStyle}/>
+                        {errors.address && <p>{errors.address.message}</p>}
+                    </div>
+                    <div style={styles.ItemStyle}>
+                        <label>City</label>
+                        <input id="city" {...register("city")} style={styles.FieldStyle}/>
+                        {errors.city && <p>{errors.city.message}</p>}
+                    </div>
+                    <div style={styles.ItemStyle}>
+                        <label>State</label>
+                        <input id="state" {...register("state")} style={styles.FieldStyle}/>
+                        {errors.state && <p>{errors.state.message}</p>}
+                    </div>
+                    <div style={styles.ItemStyle}>
+                        <label>Country</label>
+                        <input id="country" {...register("country")} style={styles.FieldStyle}/>
+                        {errors.country && <p>{errors.country.message}</p>}
+                    </div>
+                    <div style={styles.ItemStyle}>
+                        <label>Zip Code</label>
+                        <input id="zipcode" {...register("zipcode")} style={styles.FieldStyle}/>
+                        {errors.zipcode && <p>{errors.zipcode.message}</p>}
+                    </div>
+                    <div style={styles.ItemStyle}>
+                        <label>Phone</label>
+                        <input id="phone" {...register("phone")} style={styles.FieldStyle}/>
+                        {errors.phone && <p>{errors.phone.message}</p>}
+                    </div>
+                    <div style={styles.ItemStyle}>
+                        <label>Password</label>
+                        <input id="password" {...register("password")} style={styles.FieldStyle}/>
+                        {errors.password && <p>{errors.password.message}</p>}
+                    </div>
+                    <br />
                 </div>
-                <div style={styles.ItemStyle}>
-                    <label>Last Name</label>
-                    <input id="lastname" {...register("lastname")} style={styles.FieldStyle}/>
-                    {errors.lastname && <p>{errors.lastname.message}</p>}
+                <div style={styles.ButtonDivStyle}>
+                    <button type="submit" style={styles.ButtonStyle}><h3 style={styles.Header}>Create Account</h3></button>
                 </div>
-                <div style={styles.ItemStyle}>
-                    <label>Birthdate</label>
-                    <input id="birthdate" {...register("birthdate")} />
-                    {errors.birthdate && <p>{errors.birthdate.message}</p>}
-                </div>
-                <div>
-                    <label>Email</label>
-                    <input id="email" {...register("email")} />
-                    {errors.email && <p>{errors.email.message}</p>}
-                </div>
-                <div>
-                    <label>Address</label>
-                    <input id="address" {...register("address")} />
-                    {errors.address && <p>{errors.address.message}</p>}
-                </div>
-                <div>
-                    <label>City</label>
-                    <input id="city" {...register("city")} />
-                    {errors.city && <p>{errors.city.message}</p>}
-                </div>
-                <div>
-                    <label>State</label>
-                    <input id="state" {...register("state")} />
-                    {errors.state && <p>{errors.state.message}</p>}
-                </div>
-                <div>
-                    <label>Country</label>
-                    <input id="country" {...register("country")} />
-                    {errors.country && <p>{errors.country.message}</p>}
-                </div>
-                <div>
-                    <label>Zip Code</label>
-                    <input id="zipcode" {...register("zipcode")} />
-                    {errors.zipcode && <p>{errors.zipcode.message}</p>}
-                </div>
-                <div>
-                    <label>Phone</label>
-                    <input id="phone" {...register("phone")} />
-                    {errors.phone && <p>{errors.phone.message}</p>}
-                </div>
-                <div>
-                    <label>Password</label>
-                    <input id="password" {...register("password")} />
-                    {errors.password && <p>{errors.password.message}</p>}
-                </div>
-                <br />
+                
+                </form>
             </div>
-            <button type='submit'>Submit</button>
-            </form>
         </>
        
     );
