57bb0c028a433e8b918873576653ff3b0bd70bab
dmilsap <drm528@msstate.edu>
2025-03-30 20:05:56 -0500

    share and merge tree logic

---
+++ server/migrations/20250306153554_add_shared_tree_database.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema
+    .createTable('sharedTrees', (table) =>{
+        table.increments('sharedTreeID').primary();
+        table.integer('senderID').references('id').inTable('users');
+        table.integer('recieverID');
+        table.enu('perms', ['view', 'edit']);
+        table.enu('parentalSide', ['paternal', 'maternal', 'both']);
+        table.dateTime('sahreDate');
+        table.json("treeInfo");
+    } )
+
+};
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists('sharedTrees')
+};
