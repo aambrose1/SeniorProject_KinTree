ea1bdb2e4ac32bfeea923ca88372269a72ae2e34
Jade Thompson <jadethompson285@gmail.com>
2025-04-30 11:24:45 -0500

    account page updates + an attempt at active user storage

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,32 @@
 import logo from './assets/kintreelogo-adobe.png';
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { CurrentUserProvider } from './CurrentUserProvider';
 
 function App() {
   return (
-    <div className={"App font-face-alata"}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* sample web page, doesn't show anymore bc of router */}
-        <div className="Container">
-          <p>
-            Welcome to KinTree! This is the client side of the application. It's in progress.
-          </p>
-        </div>
-        <a
-          className="App-link"
-          href="https://github.com/OwenAdams2023/SeniorProject_KinTree"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github Repository
-        </a>
-      </header>
-      <Outlet />
-    </div>
+    <CurrentUserProvider>
+      <div className={"App font-face-alata"}>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {/* sample web page, doesn't show anymore bc of router */}
+          <div className="Container">
+            <p>
+              Welcome to KinTree! This is the client side of the application. It's in progress.
+            </p>
+          </div>
+          <a
+            className="App-link"
+            href="https://github.com/OwenAdams2023/SeniorProject_KinTree"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github Repository
+          </a>
+        </header>
+        <Outlet />
+      </div>
+    </CurrentUserProvider>
   );
 }
 
---
+++ client/src/CurrentUserProvider.js
@@ -0,0 +1,25 @@
+import { React, useState, createContext } from "react"
+
+export const currentContext = createContext();
+
+export const CurrentUserProvider = ({ children }) => {
+  const [currentUserID, setCurrentUserIDState] = useState(null);
+
+  const setCurrentUserID = (userID) => {
+    localStorage.setItem("currentUserID", userID);
+    setCurrentUserIDState(userID);
+  }
+
+  const fetchCurrentUser = async () => {
+    let currentUserID = localStorage.getItem("currentUserID");
+    setCurrentUserIDState(currentUserID);
+  }
+
+  return (
+    <currentContext.Provider value={{ currentUserID, setCurrentUserID, fetchCurrentUser }}>
+      {children}
+    </currentContext.Provider>
+  )
+}
+
+export const useCurrentUser = () => useContext(currentContext)
