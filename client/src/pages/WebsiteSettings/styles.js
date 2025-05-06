78d32be926ada0e9cad06e3737c452857c1a1397
Jade Thompson <jadethompson285@gmail.com>
2025-05-06 13:44:03 -0500

    fixed scrolling issue

--- client/src/pages/WebsiteSettings/styles.js
+++ client/src/pages/WebsiteSettings/styles.js
@@ -16,6 +16,7 @@ export const SettingsContainer = {
   borderRadius: '10px',
   boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
   fontFamily: 'Alata',
+  overflow: 'scroll'
 };
 
 export const Section = {
